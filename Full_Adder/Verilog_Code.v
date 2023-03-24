add603d83391d84b3853b83155dceb6cca547c79
SURA SAI SWAROOP <128355949+SURA-SAI-SWAROOP@users.noreply.github.com>
2023-03-24 12:26:06 +0530

    Create Verilog_Code.v

---
+++ Full_Adder/Verilog_Code.v
@@ -0,0 +1,13 @@
+`timescale 1ns / 1ps
+
+module full_adder(
+    input a,b,cin,
+    output reg sum,cout
+    );
+
+always @(*)
+begin
+     sum <= a ^ b ^ cin;
+     cout <= (a & b)|(b & cin)|(cin & a);
+end
+endmodule
